665d0f550db3289150e730a0137b062c0aecf3a8
ilioscio <ilioscio@gmail.com>
2020-02-18 10:54:01 -0700

    Cleaned up comments and removed commented cruft

--- common.nix
+++ common.nix
@@ -7,7 +7,7 @@
   environment.variables.EDITOR = "neovim";
   time.timeZone = "America/Boise";
 
-  # My usual account
+  # My usual user account
   users.users.ilios= {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
@@ -16,15 +16,8 @@
   # NTP clock synchronization
   services.timesyncd.enable = true;
 
-  # Filter incoming traffic
-  #networking.firewall.enable = true;
-  #networking.firewall.allowPing = true;
-  #networking.firewall.allowedTCPPorts = [ 22 ];
-  #networking.firewall.allowedUDPPorts = [ ];
-
   # Install things I'm used to having around
   environment.systemPackages = with pkgs; [
-    #(import ./nvim.nix)
     neovim
     git
     wget
